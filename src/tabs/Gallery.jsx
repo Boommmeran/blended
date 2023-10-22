c0c8a8e7857fbc70a20dae78ce9eea6c33f7335c
Andrii-13 <128071322+Andrii-13@users.noreply.github.com>
2023-10-22 12:44:33 +0300

    Update Gallery.jsx

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -7,38 +7,67 @@ export class Gallery extends Component {
   state = {
     query: '',
     page: 1,
-    images:[]
-  }
+    images: [],
+    buttonVisible: false,
+  };
 
-  getImages = async() => {
-     try {
-       const images = await ImageService.getImages(this.state.query, this.state.page)
-     
-     } catch (error) {
-      console.log("Error") 
-     }
-  }
-
+  getImages = async () => {
+    try {
+      const images = await ImageService.getImages(
+        this.state.query,
+        this.state.page
+      );
+      this.setState(prevState => ({
+        images: [...prevState.images, ...images.photos],
+        buttonVisible: images.page < Math.ceil(images.total_results/images.per_page),
+      }));
+    } catch (error) {
+      console.log('Error');
+    }
+  };
 
   componentDidUpdate(_, prevState) {
-    if (prevState.query !== this.state.query || this.state.page !== prevState.page) {
-      this.getImages()
+    if (
+      prevState.query !== this.state.query ||
+      this.state.page !== prevState.page
+    ) {
+      this.getImages();
     }
   }
-  
 
-
-  handleSubmit = (query) => {
+  handleSubmit = query => {
     this.setState({
       query,
-    })
-  }
+    });
+  };
+
+  buttonClick = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
 
   render() {
     return (
       <>
         <SearchForm handleSubmit={this.handleSubmit} />
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        <Grid>
+          {this.state.images.map(({ id, avg_color, alt, src }) => {
+            return (
+              <GridItem key={id}>
+                <CardItem color={avg_color}>
+                  <img src={src.large} alt={alt} />
+                </CardItem>
+              </GridItem>
+            );
+          })}
+        </Grid>
+        {this.state.images.length === 0 && (
+          <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        )}
+        {this.state.buttonVisible && (
+          <Button type="button" onClick={this.buttonClick}>
+            Load more
+          </Button>
+        )}
       </>
     );
   }
